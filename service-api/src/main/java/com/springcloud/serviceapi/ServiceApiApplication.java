86f8ce172eaa882c5738497971d40c0e1b54ae60
chaoxingyu <892364036@qq.com>
2017-10-26 18:27:35 +0800

    创建common和service-api

---
+++ service-api/src/main/java/com/springcloud/serviceapi/ServiceApiApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.serviceapi;
+
+import com.springcloud.common.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiApplication.class, args);
+	}
+
+	public static void test(String[] args) {
+		User user = new User();
+	}
+}
