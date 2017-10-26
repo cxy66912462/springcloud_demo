f77c364519f8c5bfd9aace9733f2b7ac50aa3dde
cxy669124622 <892364036@qq.com>
2017-10-26 22:12:35 +0800

    高可用博客服务2

---
+++ blogservice_provider2/src/main/java/com/springcloud/blogservice_provider2/BlogserviceProvider2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.blogservice_provider2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogserviceProvider2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogserviceProvider2Application.class, args);
+	}
+}
