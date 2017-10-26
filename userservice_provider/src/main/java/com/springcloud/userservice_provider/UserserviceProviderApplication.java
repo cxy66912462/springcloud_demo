2e9f8099c775cb2593e728bfa7849480b7d63670
cxy669124622 <892364036@qq.com>
2017-10-26 22:12:50 +0800

    高可用用户服务

---
+++ userservice_provider/src/main/java/com/springcloud/userservice_provider/UserserviceProviderApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.userservice_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceProviderApplication.class, args);
+	}
+}
