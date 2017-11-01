8c1e11dce8c15016104108e20b351955a8236a4f
cxy669124622 <892364036@qq.com>
2017-11-01 22:21:42 +0800

    创建高可用配置中心

---
+++ config_server2/src/main/java/com/springcloud/config_server2/ConfigServer2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.config_server2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer2Application.class, args);
+	}
+}
