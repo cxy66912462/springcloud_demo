47329353fe2f2804b3449baf046a25e2bb6a536d
cxy669124622 <892364036@qq.com>
2017-11-01 22:27:20 +0800

    创建高可用消息总线

---
+++ rabbitmq_bus/src/main/java/com/springcloud/rabbitmq_bus/RabbitmqBusApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.rabbitmq_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqBusApplication.class, args);
+	}
+}
---
+++ rabbitmq_bus/src/test/java/com/springcloud/rabbitmq_bus/RabbitmqBusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.rabbitmq_bus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqBusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
