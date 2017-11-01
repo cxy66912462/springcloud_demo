239f0bef4f9b51ee370b4d111857fda8e72774f7
cxy669124622 <892364036@qq.com>
2017-11-01 22:04:35 +0800

    创建博客服务

--- blogservice_provider/src/main/java/com/springcloud/blogservice_provider/BlogserviceProviderApplication.java
+++ blogservice_provider/src/main/java/com/springcloud/blogservice_provider/BlogserviceProviderApplication.java
@@ -2,8 +2,10 @@ package com.springcloud.blogservice_provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BlogserviceProviderApplication {
 
 	public static void main(String[] args) {
