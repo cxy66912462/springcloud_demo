fff8d42db81c0c6147b76a33b8693457a815dfef
cxy669124622 <892364036@qq.com>
2017-11-01 22:28:07 +0800

    创建高可用消息总线

---
+++ rabbitmq_bus2/src/main/java/com/springcloud/rabbitmq_bus2/RabbitmqBus2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.rabbitmq_bus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqBus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqBus2Application.class, args);
+	}
+}
