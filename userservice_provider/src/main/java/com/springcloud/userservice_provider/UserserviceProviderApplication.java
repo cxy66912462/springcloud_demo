08570c70c2a82779cff6853fae99a80c3a1b49b0
cxy669124622 <892364036@qq.com>
2017-11-01 23:17:49 +0800

    用户获取服务测试

--- userservice_provider/src/main/java/com/springcloud/userservice_provider/UserserviceProviderApplication.java
+++ userservice_provider/src/main/java/com/springcloud/userservice_provider/UserserviceProviderApplication.java
@@ -3,10 +3,17 @@ package com.springcloud.userservice_provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserserviceProviderApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceProviderApplication.class, args);
---
+++ userservice_provider/src/main/java/com/springcloud/userservice_provider/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.springcloud.userservice_provider.controller;
+
+import com.springcloud.common.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Integer id){
+        logger.info("查询用户信息,id={}",id);
+
+        User user = new User();
+        user.setId(1);
+        user.setAge(25);
+        user.setUsername("大吉大利");
+        user.setPassword("qwe");
+        user.setSalt("asd");
+
+        return user;
+    }
+
+}
