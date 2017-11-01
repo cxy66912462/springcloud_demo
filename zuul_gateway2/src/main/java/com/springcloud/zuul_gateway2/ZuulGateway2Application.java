a0f8c70f5bab27cc8b516d5416b0c6ade9a7024f
cxy669124622 <892364036@qq.com>
2017-11-01 22:29:26 +0800

    创建高可用服务网关

---
+++ zuul_gateway2/src/main/java/com/springcloud/zuul_gateway2/ZuulGateway2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.zuul_gateway2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGateway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGateway2Application.class, args);
+	}
+}
