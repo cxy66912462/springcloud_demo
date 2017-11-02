6147b17672029c6bd97432cb282423c18d55ce43
chaoxingyu <892364036@qq.com>
2017-11-02 13:42:29 +0800

    添加获取用户接口

--- userservice_provider2/src/main/java/com/springcloud/userservice_provider2/UserserviceProvider2Application.java
+++ userservice_provider2/src/main/java/com/springcloud/userservice_provider2/UserserviceProvider2Application.java
@@ -3,10 +3,17 @@ package com.springcloud.userservice_provider2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserserviceProvider2Application {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceProvider2Application.class, args);
---
+++ userservice_provider2/src/main/java/com/springcloud/userservice_provider2/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.springcloud.userservice_provider2.controller;
+
+import com.springcloud.common.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Integer id){
+        logger.info("查询用户信息,id = {} ,service = {}",id,applicationName+":"+port);
+
+        User user = new User();
+        user.setId(1);
+        user.setAge(25);
+        user.setUsername("大吉大利");
+        user.setPassword("qwe");
+        user.setSalt("asd");
+
+        return user;
+    }
+
+}
