25a3ece987861316e06dd3846e147669d0b8468b
chaoxingyu <892364036@qq.com>
2017-11-02 13:42:11 +0800

    添加获取用户接口

--- userservice_provider/src/main/java/com/springcloud/userservice_provider/controller/UserController.java
+++ userservice_provider/src/main/java/com/springcloud/userservice_provider/controller/UserController.java
@@ -4,6 +4,7 @@ import com.springcloud.common.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,15 @@ public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable Integer id){
-        logger.info("查询用户信息,id={}",id);
+        logger.info("查询用户信息,id = {} ,service = {}",id,applicationName+":"+port);
 
         User user = new User();
         user.setId(1);
